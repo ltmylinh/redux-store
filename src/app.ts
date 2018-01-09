f91e845e2703ff53be6ae5e6fb333557a86b4c5c
Linh Le <ltmylinh2606@gmail.com>
2018-01-09 15:25:18 -0500

    commit Store creation and State initial

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import { renderTodos } from './utils';
 
+import * as fromStore from './store';
+
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const store = new fromStore.Store({}, {
+  todos: [{label: 'meal', completely: false}]
+});
 
 button.addEventListener(
   'click',
@@ -12,9 +18,11 @@ button.addEventListener(
 
     const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+    //console.log(payload);
 
     input.value = '';
+
+    console.log(store.value);
   },
   false
 );
